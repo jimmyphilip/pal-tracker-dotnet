4cca9bb526781e89cc17ed977943c95d9d5f2b12
jimmyphilip <jimmy.philip@schwab.com>
2018-07-31 12:03:28 -0600

    Simple .NET Core Web API app - changes 12

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -39,9 +40,10 @@ namespace PalTracker
                                              "Cf instance addr not set.")
      
            ));
-
-           services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
-
+                        
+            //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();            
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
